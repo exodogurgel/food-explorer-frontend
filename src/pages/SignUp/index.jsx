a3d1ec3b99f617b4e0626420f40dbbacfe6f2a42
Êxodo Gurgel <exodowellis@gmail.com>
2022-08-17 12:44:26 -0300

    finish SignUp

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,37 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { api } from '../../services/api';
+
 import { Container, Form } from './styles';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import { Link } from 'react-router-dom';
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos");
+    }
+
+    api.post("/users", {name, email, password})
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!");
+        navigate("/");
+      })
+      .catch(error => {
+        if(error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      })
+  }
+
   return (
     <Container>
       <div>
@@ -20,6 +48,7 @@ export function SignUp() {
           label="name" 
           title="Seu nome" 
           placeholder="Exemplo: Maria da Silva"
+          onChange={e => setName(e.target.value)}
           required  
         />
 
@@ -28,6 +57,7 @@ export function SignUp() {
           label="email" 
           title="Email" 
           placeholder="Exemplo: exemplo@exemplo.com"
+          onChange={e => setEmail(e.target.value)}
           required
         />
         
@@ -36,11 +66,15 @@ export function SignUp() {
           label="password" 
           title="Senha" 
           placeholder="No mínimo 6 caracteres"
+          onChange={e => setPassword(e.target.value)}
           minlength="6"
           required
         />
 
-        <Button title="Criar conta"/>
+        <Button
+          title="Criar conta"
+          onClick={handleSignUp}
+        />
 
         <Link to="/">Já tenho uma conta</Link>
       </Form>
